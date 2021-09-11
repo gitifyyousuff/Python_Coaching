74c424dffbfae344386ea4e2205f1d1da6759483
Yousuff <56258703+gitifyyousuff@users.noreply.github.com>
2021-09-11 22:25:01 +0530

    Add files via upload

---
+++ Python_Fundamental_part3/anotherModule.py
@@ -0,0 +1,10 @@
+def sq(x):
+    res_sq = 45
+    return res_sq
+
+def cub(x):
+    res_cu = x*x*x
+    return res_cu
+
+
+    
---
+++ Python_Fundamental_part3/ownModule.py
@@ -0,0 +1,10 @@
+def sq(x):
+    res_sq = x*x
+    return res_sq
+
+def cub(x):
+    res_cu = x*x*x
+    return res_cu
+
+
+    
